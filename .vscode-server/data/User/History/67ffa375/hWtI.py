22c7e71d3587236c1ebdc85dfa3b11136f828aa0
tjdnf4415 <min983106@gmail.com>
2024-09-08 12:45:59 +0000

    01

--- .vscode-server/data/User/History/67ffa375/hWtI.py
+++ .vscode-server/data/User/History/67ffa375/hWtI.py
@@ -53,21 +53,3 @@ def question_create(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'myapp/question_form.html', context)
-
-@login_required(login_url='common:login')
-def question_modify(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    if request.user != question.author:
-        messages.error(request, '수정권한이 없습니다')
-        return redirect('myapp:detail', question_id=question.id)
-    if request.method == "POST":
-        form = QuestionForm(request.POST, instance=question)
-        if form.is_valid():
-            question = form.save(commit=False)
-            question.modify_date = timezone.now()  # 수정일시 저장
-            question.save()
-            return redirect('myapp:detail', question_id=question.id)
-    else:
-        form = QuestionForm(instance=question)
-    context = {'form': form}
-    return render(request, 'myapp/question_form.html', context)
--- .vscode-server/data/User/History/67ffa375/t4iR.py
+++ .vscode-server/data/User/History/67ffa375/t4iR.py
@@ -59,15 +59,15 @@ def question_modify(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.user != question.author:
         messages.error(request, '수정권한이 없습니다')
-        return redirect('myapp:detail', question_id=question.id)
+        return redirect('pybo:detail', question_id=question.id)
     if request.method == "POST":
         form = QuestionForm(request.POST, instance=question)
         if form.is_valid():
             question = form.save(commit=False)
             question.modify_date = timezone.now()  # 수정일시 저장
             question.save()
-            return redirect('myapp:detail', question_id=question.id)
+            return redirect('pybo:detail', question_id=question.id)
     else:
         form = QuestionForm(instance=question)
     context = {'form': form}
-    return render(request, 'myapp/question_form.html', context)
+    return render(request, 'pybo/question_form.html', context)
